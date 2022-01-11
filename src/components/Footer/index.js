20ef8acca9188bd316fab07c117c3c816db98c51
AS <asadrgilany7@gmail.com>
2022-01-10 21:30:40 -0500

    adding components

---
+++ src/components/Footer/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Footer() {
+    return (
+        <footer>
+            <a href="https://github.com/asadg7">
+                <img src="/assets/images/icons/github.png" />
+            </a>
+            <a href="https://www.linkedin.com/">
+                <img src="/assets/images/icons/linkedin.png" />
+            </a>
+            <a href="https://stackoverflow.com/users/16594614/dark-light">
+                <img src="/assets/images/icons/stackoverflow.png" />
+            </a>
+        </footer>
+    )
+};
+
+export default Footer;
---
+++ src/components/Header/index.js
@@ -0,0 +1,19 @@
+import React, { useEffect } from "react";
+import Navigation from "../Navigation";
+
+function Header() {
+    useEffect(() => {
+        
+    });
+
+    return (
+        <header>
+            <h2>
+                Alex
+            </h2>
+            <Navigation />
+        </header>
+    );
+};
+
+export default Header;
